c73e5d3e67c1e959fdc6a1b81337841b15297f08
Richard Kim <richardjkim7@gmail.com>
2022-12-01 10:44:51 -0600

    Spring practice

---
+++ src/main/java/com/codeup/spring_practice/HelloController.java
@@ -0,0 +1,14 @@
+package com.codeup.spring_practice;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hi")
+    @ResponseBody
+    public String hi() {
+        return "Hi from Spring!";
+    }
+}
